a78d6d470bb5128b4c481439074744e7ce4f39bd
Your Name <you@example.com>
2023-07-13 15:09:33 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,9 @@
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
-{
-char quo =[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+{char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
 writ(1, quo, 59);
-return (1)
-}	
+return (1);
+}
