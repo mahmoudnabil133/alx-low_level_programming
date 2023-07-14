e281799bd45fde3976935cefbaabdd752b6b156b
Your Name <you@example.com>
2023-07-14 16:13:47 -0700

    fixed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+if (a == 'q' || a == 'e')
+{
+a++;
+continue;
+}
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
