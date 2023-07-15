da629800d96c4c77aa1bfa717371140a85201f74
Your Name <you@example.com>
2023-07-14 17:34:43 -0700

    fixed

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a = '0';
+while (a < '@')
+{
+if (a > '9')
+{
+putchar(a + 39);
+a++;
+continue;
+}
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
