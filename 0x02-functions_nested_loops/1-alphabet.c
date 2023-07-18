355e6b007eb3c9fd40e5277006cd31f92bcbbca8
Your Name <you@example.com>
2023-07-18 15:19:14 -0700

    fixed commit#

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - print alphbet.
- *
- * Return: Always 0.
+ * print_alphabet - print all alphabet in lowercase
  */
 
 void print_alphabet(void)
 {
-char a;
-for (a = 'a'; a <= 'z'; a++)
-_putchar(a);
-_putchar('\n');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
+
+
