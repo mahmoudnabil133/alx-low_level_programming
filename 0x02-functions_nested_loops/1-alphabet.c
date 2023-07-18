7ecdb079d769d5f96110323e7b38c37541afe041
Your Name <you@example.com>
2023-07-18 13:49:48 -0700

    fixed commit#

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main"
+/**
+ * main - print alphbet.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphbet(void)
+{
+	int a = 'a';
+	for (a; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	putchar('\n');
+}
