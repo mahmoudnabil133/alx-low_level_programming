a106776e40b78557c03e4e69f6a402f76df6039c
Your Name <you@example.com>
2023-07-18 16:00:33 -0700

    fixed commit#

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
- * print the alphabet - in lowercase ten times
- *
+ *print_alphabet_x10 - print alphbets x10 in lowercase
  * Return: Always 0.
  *
  */
 
 void print_alphabet_x10(void)
-{
-	int i;
+{int i;
 	for (i = 0; i < 10; i++)
-	{
-		char ch;
+	{char ch;
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 	_putchar('\n');
