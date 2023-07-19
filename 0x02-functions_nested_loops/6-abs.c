75d86337c64895b1880fed98c2e4354130e9ba35
Your Name <you@example.com>
2023-07-18 17:40:37 -0700

    fixed commit#

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * _abs - gets the absolute of the passed number
+ *
+ * @n : the number to get it,s abs
+ *
+ * Return: 0 Always
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-n);
+}
