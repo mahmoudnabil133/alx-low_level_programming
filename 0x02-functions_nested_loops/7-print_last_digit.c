1ede6127ee37944692307c2a4ac15834ecb973a9
Your Name <you@example.com>
2023-07-18 17:53:19 -0700

    fixed commit#

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - it prints last dig
+ *
+ * @n : the argument passed
+ *
+ * Return: the last diget
+ */
+int print_last_digit(int n)
+{int l;
+	l = n % 10;
+	return (l);
+}
