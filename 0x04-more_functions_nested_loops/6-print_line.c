0a4e610b452ca420cb4203c1ea56c2a1db80e16d
Your Name <you@example.com>
2023-07-20 10:51:25 -0700

    fixed com@@

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_line -  print a line
+ * @n: the lenght of the line
+ */
+void print_line(int n)
+{int i;
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
