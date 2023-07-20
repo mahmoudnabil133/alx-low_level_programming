d4c6bb6419de45ee2cc97a2e6f00dc8799035653
Your Name <you@example.com>
2023-07-20 13:40:27 -0700

    fixed com@@

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@ void print_triangle(int size)
 {int i, j, k;
 	for (i = 0; i < size; i++)
 	{
-		for (j = size - 1; j > i; j--)
+		for (j = size; j > i; j--)
 			_putchar(' ');
 		for (k = 0; k < i + 1; k++)
 			_putchar('#');
