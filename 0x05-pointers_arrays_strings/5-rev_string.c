2567db037360530d6e640ae4a4992ee2cbba2566
Your Name <you@example.com>
2023-07-24 11:37:02 -0700

    fixed com@@

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverse a string
+ * @s: string will be reversed
+ */
+void rev_string(char *s)
+{int len ,i;
+	len = strlen(s);
+	for (i = len - 1; i >= 0; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
