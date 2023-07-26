b9ae36530e21c1e4540cc29794a31861f80b4f3b
Your Name <you@example.com>
2023-07-26 15:24:35 -0700

    fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,9 +6,9 @@
  */
 char *cap_string(char *s)
 {int i;
-i = 0;
-while (s[i] != '\0')
-{
+	i = 0;
+	while (s[i] != '\0')
+	{
 	if (s[i + 1] != '\0' && (s[i] == '\n' || s[i] == '\t'))
 		s[i + 1] = s[i + 1] - 32;
 	if (s[i] >= 'a' && s[i] <= 'z' && s[i - 1] == '.')
@@ -17,5 +17,5 @@ while (s[i] != '\0')
 		s[i] = s[i] - 32;
 	i++;
 	return (s);
-}
+	}
 }
