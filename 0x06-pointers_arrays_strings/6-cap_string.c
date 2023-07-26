9204ad6a4237a11488c582082cc24d7ac47562d4
Your Name <you@example.com>
2023-07-26 14:58:54 -0700

    fixed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - convert from lower to upper
+ * @s: the string will be uppered
+ * Return: the uppercase string
+ */
+char *string_toupper(char *s)
+{int i;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i + 1] != '\0' && (s[i] == '\n' || s[i] == '\t'))
+		       	s[i + 1] = s[i + 1] - 32;
+		if (s[i] >= 'a' && s[i] <= 'z' && ((s[i - 1] == ' ') || (s[i - 1] == '.')))
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
