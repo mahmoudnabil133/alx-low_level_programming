0bf9eee4d4d40366f6cd68109abf4a2b5e195650
Your Name <you@example.com>
2023-07-26 16:06:11 -0700

    fixed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,21 +5,26 @@
  * Return: char
  */
 char *leet(char *s)
-{int i;
+{int i, b;
+	b = 1;
 	i = 0;
-	while (s[i] != '\0')
+	while (b)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = 52;
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = 51;
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = 48;
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = 55;
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = 49;
-		i++;
+		while (s[i] != '\0')
+		{
+			if (s[i] == 'a' || s[i] == 'A')
+				s[i] = 52;
+			if (s[i] == 'e' || s[i] == 'E')
+				s[i] = 51;
+			if (s[i] == 'o' || s[i] == 'O')
+				s[i] = 48;
+			if (s[i] == 't' || s[i] == 'T')
+				s[i] = 55;
+			if (s[i] == 'l' || s[i] == 'L')
+				s[i] = 49;
+			i++;
+		}
+		b = 0;
 	}
 	return (s);
 }
