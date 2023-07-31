dce39d0413ea7a1f11cb663f06579513cac692a1
Your Name <you@example.com>
2023-07-31 14:22:53 -0700

    fixed com@@

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_chessboard - it prints achessboard
+ * @a: the array will be printed
+ */
+void print_chessboard(char (*a)[8])
+{int i, j;
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			printf("%c", a[i][j]);
+		printf("\n");
+	}
+}
