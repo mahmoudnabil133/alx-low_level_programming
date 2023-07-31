bec3d2d9525dcc43ad1497673a8e3e9971c34fd0
Your Name <you@example.com>
2023-07-31 11:03:09 -0700

    fixed com@@

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strchr - return a pointer to the first occurance of string
+ * @c: the character will be passed to the function
+ * Return: string from the first char c in the string
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c););
+}
+
