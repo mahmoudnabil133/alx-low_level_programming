2d9df3b4830e4546367a855ec5edfa306dded515
Your Name <you@example.com>
2023-08-02 13:24:09 -0700

    fixed com@@

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _print_rev_recursion - it print a rev  string with rec
+ * @s: the string will be passed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*s);
+}
+
