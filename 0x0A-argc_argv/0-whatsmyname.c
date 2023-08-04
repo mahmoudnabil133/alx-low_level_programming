74a189fad4778ccc5574fce4d6e40fe65f38fc52
Your Name <you@example.com>
2023-08-04 14:30:32 -0700

    fixed for argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - playing with its arguments
+ * @argc: the num of arg passed to main
+ * @argv: array of string of args passed to main
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0 * argc]);
+	return (0);
+}
