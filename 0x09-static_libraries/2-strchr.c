e956cc4bbf8b5b93911dc2c3b6c115ba14c6b6a1
Your Name <you@example.com>
2023-08-04 11:18:37 -0700

    fixed for s_lib

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - return a pointer to the first occurance of string
+ * @s : original string
+ * @c: the character will be passed to the function
+ * Return: string from the first char c in the string
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
+
---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - it compares 2 str
+ * @s1: str1
+ * @s2: str2
+ * Return: 0 if they are same less than 0 if s1<s2 bigger than 0 if s1>s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
