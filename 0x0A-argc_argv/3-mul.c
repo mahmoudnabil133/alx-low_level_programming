3a17936692221128cff0f84c50fff9e26d519cc1
Your Name <you@example.com>
2023-08-04 15:17:02 -0700

    fixed for argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,16 @@
  */
 int main(int argc, char *argv[])
 {int cnt, mul = 1;
-	if (argc > 2)
+	if (argc > 1)
 	{
 		for (cnt = 1; cnt < argc; cnt++)
 			mul *= atoi(argv[cnt]);
 		printf("%d\n", mul);
 	}
 	else
+	{
 		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }
