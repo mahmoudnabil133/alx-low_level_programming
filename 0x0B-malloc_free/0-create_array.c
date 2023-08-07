ed0369e39bdacb1a34637304da47b75bfb1f9f5c
Your Name <you@example.com>
2023-08-07 12:26:50 -0700

    malloc-free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * create_array - it creates array
+ * @size: size of arr
+ * @c: the char
+ * Return: array
+ */
+
+char *create_array(unsigned int size, char c)
+{char *s;
+	s = (char *) malloc(size * sizeof(char));
+	int i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
