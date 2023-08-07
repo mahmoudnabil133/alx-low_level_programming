fdc8d9e6b98752a0f75427d57269a89c51c82a85
Your Name <you@example.com>
2023-08-07 13:41:00 -0700

    malloc-free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,10 +10,10 @@ char *_strdup(char *str)
 	char *str2;
 	n = strlen(str);
 	str2 = malloc(n * sizeof(char));
-	int i = 0;
+	int i;
+	i = 0;
 	while (i < n)
-	{
-		str2[i] = str[i];
+	{str2[i] = str[i];
 		i++;
 	}
 	str2[i] = '\0';
