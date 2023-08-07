71c3bd4a4dca539b13ebad53bc61979ca393ac0d
Your Name <you@example.com>
2023-08-07 12:54:52 -0700

    malloc-free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,11 +9,16 @@ char *create_array(unsigned int size, char c)
 {char *s;
 	unsigned int i;
 	i = 0;
-	s = (char *) malloc(size * sizeof(char));
+	s =  malloc(size * sizeof(char));
 	while (i < size)
+	{
 		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
 	if (size == 0)
 		return (NULL);
 	else
 		return (s);
+	free(s);
 }
