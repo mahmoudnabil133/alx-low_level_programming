3acad1829e93615b876f6aab6a654ec6fddc69ef
Your Name <you@example.com>
2023-08-10 16:46:47 -0700

    0x0C-m,more_malloc_free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,9 +11,9 @@ int *array_range(int min, int max)
 		return (NULL);
 	sum = max - min + 1;
 	ptr = malloc(sum * sizeof(int));
-	for (i = min, j = 0; i <= max; i++, j++)
-		ptr[j] = i;
 	if (ptr == NULL)
 		return (NULL);
+	for (i = min, j = 0; i <= max; i++, j++)
+		ptr[j] = i;
 	return (ptr);
 }
