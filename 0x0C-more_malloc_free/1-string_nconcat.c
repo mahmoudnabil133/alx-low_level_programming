edea28378c2cd86c64e671ed783c4766ad67c445
Your Name <you@example.com>
2023-08-10 15:25:44 -0700

    0x0C-m,more_malloc_free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * string_nconcat - it concats 2 strings
+ * @s1: string1
+ * @s2: string2
+ * @n: number of char will be concated from str2
+ * Return: char pointer to a concatinated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{unsigned int len1, len2, i, sum;
+	char *c;
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n >= len2)
+		n = len2;
+	sum = len1 + n + 1;
+	c = malloc(sizeof(char) * sum);
+	for (i = 0; i < len1; i++)
+		c[i] = s1[i];
+	for (i = 0; i < n; i++)
+		c[i + len1] = s2[i];
+	c[sum - 1] = '\0';
+	if (c == NULL)
+		return (NULL);
+	return (c);
+}
+
+
