e2ee0e77c252366b0f6f61c8e50bf920aa04b061
Your Name <you@example.com>
2023-08-10 17:00:38 -0700

    0x0C-m,more_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,31 +1,48 @@
+#include <stdlib.h>
 #include "main.h"
-/**
- * string_nconcat - it concats 2 strings
- * @s1: string1
- * @s2: string2
- * @n: number of char will be concated from str2
- * Return: char pointer to a concatinated string
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{unsigned int len1, len2, i, sum;
-	char *c;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	if (n >= len2)
-		n = len2;
-	sum = len1 + n + 1;
-	c = malloc(sizeof(char) * sum);
-	if (c == NULL)
-		return (NULL);
-	for (i = 0; i < len1; i++)
-		c[i] = s1[i];
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
 	for (i = 0; i < n; i++)
-		c[i + len1] = s2[i];
-	c[sum - 1] = '\0';
-	return (c);
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
 
 
