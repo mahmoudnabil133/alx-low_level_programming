fe24152f316b810d9a7a5bc5fb8913908b1dfe07
Your Name <you@example.com>
2023-08-16 16:41:29 -0700

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - show the index wich match func that cmp_pointer points to
+ * @array: the arr we iterate
+ * @size: size of arr
+ * @cmp: function pointer.
+ * Return: index of matched num or -1 if size<=0 or no matching element.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{int i, c;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		c = cmp(array[i]);
+		if (c != 0)
+			return (i);
+	}
+	return (-1);
+}
