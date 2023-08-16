45a007824638baf0acd1d3d62d887536792e4d0d
Your Name <you@example.com>
2023-08-16 16:58:14 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,11 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {int i, c;
 	if (size <= 0)
 		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (0);
 	for (i = 0; i < size; i++)
 	{
 		c = cmp(array[i]);
 		if (c != 0)
 			return (i);
 	}
-	return (-1);
+		return (-1);
 }
