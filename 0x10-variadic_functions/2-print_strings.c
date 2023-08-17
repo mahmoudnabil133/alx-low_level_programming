17bc7cd56b19409b53471360298a2b2fa2a4ce0f
Your Name <you@example.com>
2023-08-17 13:42:45 -0700

    0x0F-function_pointers

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - it print a string
+ * @separator: the sep
+ * @n: num of optional args
+ * Return: nothing.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{unsigned int i;
+	char *s;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			s = "nil";
+		printf("%s", s);
+		if (i != (n - 1) && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
