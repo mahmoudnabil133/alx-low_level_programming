4b2f80f14b71b42ff32285cb50455451b27d6245
Your Name <you@example.com>
2023-08-17 15:40:41 -0700

    0x0F-function_pointers

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+/**
+ * print_all - it prints all
+ * @format: the key of data type of variables.
+ * Return: nothing.
+ */
+void print_all(const char * const format, ...)
+{int i = 0;
+	char *s, *sep = "";
+	va_list ap;
+
+	if (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'f':
+				printf("%s%f", sep, va_arg(ap, double));
+				break;
+			case 'c':
+				printf("%s%c", sep, va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(ap, int));
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", sep, s);
+				break;
+			default:
+				break;
+		}
+		sep = ", ";
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
