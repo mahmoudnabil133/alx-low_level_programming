ff4160aa192ab7439dfae47df9d902113af7e79f
Your Name <you@example.com>
2023-08-17 13:29:31 -0700

    0x0F-function_pointers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,14 +10,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int x;
 	va_list ap;
 
-	if (separator == NULL)
-		return;
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(ap, int);
 		printf("%d", x);
-		if (i != (n - 1))
+		if (i != (n - 1) && separator)
 			printf("%s", separator);
 	}
 	printf("\n");
