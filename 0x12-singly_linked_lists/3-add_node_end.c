58d054b8d528ac0d64b45a3b6f28f079d619700c
mahmoudnabil133 <hodanabil144@gmail.com>
2023-08-25 08:18:11 -0700

    linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node, *tail = *head;
 
 	while (tail != NULL)
-		tail =tail->next;
+		tail = tail->next;
 
 	len1 = strlen(str);
 	new_node = malloc(sizeof(list_t));
