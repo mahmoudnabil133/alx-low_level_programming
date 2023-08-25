2c3e007fb5c9dfec50699d332599e0206214d54a
mahmoudnabil133 <hodanabil144@gmail.com>
2023-08-25 08:15:40 -0700

    linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,18 +6,18 @@
  * Return: ptr to head.
 */
 list_t *add_node_end(list_t **head, const char *str)
-{unsigned int len;
+{unsigned int len1;
 	list_t *new_node, *tail = *head;
 
 	while (tail != NULL)
 		tail =tail->next;
 
-	len = strlen(str);
+	len1 = strlen(str);
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 	new_node->str = strdup(str);
-	new_node->len = len;
+	new_node->len = len1;
 	new_node->next = NULL;
 	if (*head == NULL)
 	{
