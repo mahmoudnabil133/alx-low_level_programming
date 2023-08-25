4f9e560ce3f066fdb8f7ff70842b40f2034cb6da
mahmoudnabil133 <hodanabil144@gmail.com>
2023-08-25 08:41:26 -0700

    linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,10 +6,12 @@
 */
 void free_list(list_t *head)
 {
+	list_t *temp;
 	while (head !=  NULL)
 	{
+		temp = head->next;
 		free(head->str);
 		free(head);
-		head = head->next;
+		head = temp;
 	}
 }
