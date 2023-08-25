b9475208593d26d83f6793eb643ad733df3ff764
mahmoudnabil133 <hodanabil144@gmail.com>
2023-08-25 08:49:07 -0700

    linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,13 +2,12 @@
 /**
  * free_lists - it free lists.
  * @head: head ptr
- * Return: nothing.
 */
 void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head !=  NULL)
+	while (head)
 	{
 		temp = head->next;
 		free(head->str);
