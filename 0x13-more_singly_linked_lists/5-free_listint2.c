dcf3fec0bc485d61bb5a2f8768ccd1721a69474c
mahmoudnabil133 <hodanabil144@gmail.com>
2023-08-29 06:10:19 -0700

    moreLinkedList

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,12 +4,15 @@
  * @head: head of the list.
 */
 void free_listint2(listint_t **head)
-{listint_t *temp = *head;
+{listint_t *temp;
+
+	if (*head == NULL)
+		return;
 	while (*head)
 	{
+		temp = *head;
 		temp = temp->next;
 		free(*head);
 		*head = temp;
 	}
-	*head = NULL;
 }
