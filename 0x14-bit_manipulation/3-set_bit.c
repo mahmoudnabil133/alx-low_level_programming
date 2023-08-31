bbe43161b462500cde165950b11c96a7cb1801f3
mahmoudnabil133 <hodanabil144@gmail.com>
2023-09-01 00:56:47 +0300

    bit man

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * set_bit - set a bit.
+ * @n: nummber we will set a bit in.
+ * @index: the index of bit we will set.
+ * Return: num after setting.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{unsigned long int current, inc = 1, i;
+	int res;
+
+	if (index > 63)
+		return (-1);
+	for (i = 0; i < index; i++)
+		inc *= 2;
+	current = *n >> index;
+	if (current & 1)
+		inc = 0;
+	*n += inc;
+	return (1);
+}
