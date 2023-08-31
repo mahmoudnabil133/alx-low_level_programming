401344a17b3e70049fbd9ee82ee3689835f9a16c
mahmoudnabil133 <hodanabil144@gmail.com>
2023-08-31 14:19:32 -0700

    bit man

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * get_bet - it gets val of bet we want.
+ * get_bit - it gets val of bet we want.
  * @n: num we will get a bit from.
  * @index: index of bit we want.
  * Return: val of bit or -1 if failed.
@@ -8,10 +8,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {unsigned long int current;
 
-        if (index > 63)
-                return (-1);
-        current = n >> index;
-        if (current & 1)
-                return (1);
-        return (0);
+	current = n >> index;
+	if (index > 63)
+		return (-1);
+	if (current & 1)
+		return (1);
+	return (0);
 }
