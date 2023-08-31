d4e2e005e1a82cf463435b2c0e3b2661e428a8ba
mahmoudnabil133 <hodanabil144@gmail.com>
2023-08-31 22:31:31 +0300

    bit man

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,15 +7,13 @@
 unsigned int binary_to_uint(const char *b)
 {unsigned int dec = 0;
 	int i;
-	double j = 0.0;
 	if (b == NULL)
 		return (0);
 	for (i = strlen(b) - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		dec += ((b[i] - '0') * pow (2.0, j));
-		j++;
+		dec = 2 * dec + (b[i] - '0');
 	}
 	return (dec);
 }
