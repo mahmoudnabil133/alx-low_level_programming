4d703a77131fca0952cb7631f51165799e8592a1
mahmoudnabil133 <hodanabil144@gmail.com>
2023-08-31 12:36:10 -0700

    bit man

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	for (i = strlen(b) - 1; i >= 0; i--)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
