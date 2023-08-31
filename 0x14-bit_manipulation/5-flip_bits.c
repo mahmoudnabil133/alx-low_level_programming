0e8d1be25d81c51dd9da572f22b270f558410c06
mahmoudnabil133 <hodanabil144@gmail.com>
2023-09-01 02:13:48 +0300

    bit man

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - it show changes counts to convert a num to another.
+ * @n: num1.
+ * @m: num2.
+ * Return: count of changes.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{unsigned int count = 0;
+	int i = 64;
+	unsigned long int current ,Xor = n ^ m;
+	while (i--)
+	{
+		current = Xor >> i;
+		if (current & 1)
+		       count ++;
+	}
+	return (count);
+}	
+
