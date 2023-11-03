df8c75749c4c74a0c69d8d7d93d27d0f577e3c4a
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-03 22:38:23 +0200

    HT

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - it deletes a hash table.
+ * @ht - ptr to hash table.
+ * Return: nothing.
+*/
+void hash_table_delete(hash_table_t *ht)
+{unsigned long int i;
+    hash_node_t *current, *temp;
+    if (!ht)
+        return;
+    for (i = 0; i < ht->size; i++)
+    {
+        current = ht->array[i];
+        while(current)
+        {
+            temp = current->next;
+            free_node(current);
+            current = temp;
+        }
+
+    }
+    
+
+}
