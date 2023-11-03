a05cf00aa79fc4937893620bf79cfc1b6505dd2e
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-03 23:25:17 +0200

    HT

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,23 +5,23 @@
 */
 void hash_table_print(const hash_table_t *ht)
 {unsigned long int i, x = 0;
-    hash_node_t *current;
+	hash_node_t *current;
 
-    if (!ht)
-        return;
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        current = ht->array[i];
-        while(current)
-        {
-            if (x == 1)
-                printf(", ");
-            printf("\'%s\': \'%s\'", current->key, current->value);
-            current = current->next;
-            x = 1;
-        }
-    }
-    printf("}\n");
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			if (x == 1)
+				printf(", ");
+			printf("\'%s\': \'%s\'", current->key, current->value);
+			current = current->next;
+			x = 1;
+		}
+	}
+	printf("}\n");
 
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,18 +1,19 @@
 #include "hash_tables.h"
 /**
  * hash_table_delete - it deletes a hash table.
- * @ht - ptr to hash table.
+ * @ht: ptr to hash table.
  * Return: nothing.
 */
 void hash_table_delete(hash_table_t *ht)
 {unsigned long int i;
 	hash_node_t *current, *temp;
+
 	if (!ht)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
 		current = ht->array[i];
-		while(current)
+		while (current)
 		{
 			temp = current->next;
 			free_node(current);
@@ -21,6 +22,4 @@ void hash_table_delete(hash_table_t *ht)
 	}
 	free(ht->array);
 	free(ht);
-	
-
 }
